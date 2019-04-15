69bac1f2df5695a0ff7d9a72b1090ad6d01c4f16
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2019-04-15 09:48:44 +0300

    parse xml to json

---
+++ WorkingWithFiles/files.py
@@ -0,0 +1,88 @@
+"""
+Files library.
+"""
+
+import os
+import xml.etree.ElementTree as xml
+import json
+
+
+class Person(object):
+    """__init__() functions as the class constructor"""
+
+    def __init__(self, first_name, last_name, year, month, day, company, project, role, room, hobby):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year = year
+        self.month = month
+        self.day = day
+        self.company = company
+        self.project = project
+        self.role = role
+        self.room = room
+        self.hobby = hobby
+
+
+def dataFileProcessing(path_to_source_file, path_to_destination_file, data):
+    # full_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path_to_source_file)
+    if isFileExist(path_to_source_file):
+        print("File exists")
+        tree = xml.parse(path_to_source_file)
+        root = tree.getroot()
+
+        # changing fields text
+        i = 0
+        for item in root.findall('PERSON'):
+            item.find('FIRST_NAME').text = data[i].first_name
+            item.find('LAST_NAME').text = data[i].last_name
+            item.find('YEAR_OF_BIRTH').text = data[i].year
+            item.find('MONTH_OF_BIRTH').text = data[i].month
+            item.find('DAY_OF_BIRTH').text = data[i].day
+            item.find('COMPANY').text = data[i].company
+            item.find('PROJECT').text = data[i].project
+            item.find('ROLE').text = data[i].role
+            item.find('ROOM').text = data[i].room
+            item.find('HOBBY').text = data[i].hobby
+            i += 1
+
+        # write changes back to xml file
+        tree.write(path_to_source_file)
+
+        # convert xml to json
+        obj = parseXmlToObj((xml.parse(path_to_source_file)).getroot())
+        result = {'PERSONS': obj}
+        with open(path_to_destination_file, "w") as write_file:
+            json.dump(result, write_file, indent=4)
+    else:
+        print("File doesn't exist.")
+
+
+def isFileExist(path_to_file):
+    return os.path.isfile(path_to_file)
+
+
+def parseXmlToObj(xml):
+    items = []
+    for child in list(xml):
+        item = {}
+        if len(list(child)) > 0:
+            for element in list(child):
+                item[element.tag] = element.text
+        else:
+            item[child.tag] = child.text or ''
+        items.append(item)
+    return items
+
+
+def main():
+    source_path = "samples\\xml\\test_data.xml"
+    destination_path = "samples\\json\\updated_test_data.json"
+    person_list = [Person("First1", "Last1", "2001", "Jan", "1", "company1", "project1", "role1", "room#1", "hobby1"),
+                   Person("First2", "Last2", "2002", "Jan", "2", "company2", "project2", "role2", "room#2", "hobby2"),
+                   Person("First3", "Last3", "2003", "Jan", "3", "company3", "project3", "role3", "room#3", "hobby3")]
+    dataFileProcessing(source_path, destination_path, person_list)
+
+
+if __name__ == "__main__":
+    # calling main function
+    main()
---
+++ WorkingWithFiles/test_files.py
@@ -0,0 +1,20 @@
+"""
+Unit tests for the files library
+"""
+
+import WorkingWithFiles.files as f
+import pytest
+
+from WorkingWithFiles.files import Person
+
+
+class TestWorkingWithFiles:
+
+    @pytest.mark.parametrize("path", ["samples\\xml\\test_data.xml", "samples\\json\\updated_test_data.json"])
+    def test_isFileExist(self, path):
+        person_list = [
+            Person("First1", "Last1", "2001", "Jan", "1", "company1", "project1", "role1", "room#1", "hobby1"),
+            Person("First2", "Last2", "2002", "Jan", "2", "company2", "project2", "role2", "room#2", "hobby2"),
+            Person("First3", "Last3", "2003", "Jan", "3", "company3", "project3", "role3", "room#3", "hobby3")]
+        f.dataFileProcessing("samples\\xml\\test_data.xml", "samples\\json\\updated_test_data.json", person_list)
+        assert f.isFileExist(path)
