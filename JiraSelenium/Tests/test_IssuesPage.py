2c24ac1df22bebd7d941663439d2a9d832de8eaf
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2019-04-25 16:56:35 +0300

    add rerun

--- JiraSelenium/Tests/test_IssuesPage.py
+++ JiraSelenium/Tests/test_IssuesPage.py
@@ -6,6 +6,7 @@ from JiraSelenium.Pages.CreateIssuePage import Issue
 
 class TestIssuesPage(TestTemplate):
 
+    @pytest.mark.flaky(reruns=3)
     def test_create_issue_positive(self):
         assert self.issues_page.is_at('- Hillel IT School JIRA')
         assert self.issues_page.is_user_details_visible()
@@ -13,6 +14,7 @@ class TestIssuesPage(TestTemplate):
         self.issues_page.create_issue(issue)
         assert self.issues_page.is_issue_created()[0]
 
+    @pytest.mark.flaky(reruns=3)
     def test_create_issue_negative_required_field_missing(self):
         assert self.issues_page.is_at('- Hillel IT School JIRA')
         assert self.issues_page.is_user_details_visible()
@@ -20,6 +22,7 @@ class TestIssuesPage(TestTemplate):
         self.issues_page.create_issue(issue)
         assert self.create_issue_page.is_error_displayed("You must specify a summary of the issue.")
 
+    @pytest.mark.flaky(reruns=3)
     def test_create_issue_negative_long_field(self):
         assert self.issues_page.is_at('- Hillel IT School JIRA')
         assert self.issues_page.is_user_details_visible()
@@ -37,6 +40,7 @@ class TestIssuesPage(TestTemplate):
         self.issues_page.create_issue(issue)
         assert self.create_issue_page.is_error_displayed("Summary must be less than 255 characters.")
 
+    @pytest.mark.flaky(reruns=3)
     def test_search_issue(self):
         criteria = Issue('Webinar (WEBINAR)', 'Bug', 'For search test', 'High', 'ValeriiSokolovskyi')
         self.issues_page.create_issue(criteria)
@@ -46,6 +50,7 @@ class TestIssuesPage(TestTemplate):
         assert criteria.summary in self.issues_page.get_row_content(0)
         # assert self.issues_page.get_results_count() == 1
 
+    @pytest.mark.flaky(reruns=5)
     def test_search_no_issue_found(self):
         criteria = Issue('Webinar (WEBINAR)', 'Bug', '###WILL NOT BE FOUND###', '', '')
         self.issues_page.search(criteria)
@@ -57,6 +62,7 @@ class TestIssuesPage(TestTemplate):
         Issue('Webinar (WEBINAR)', 'Bug', 'Bug for update test', 'Low', 'ValeriiSokolovskyi'),
         Issue('Webinar (WEBINAR)', 'Bug', 'Bug for update test', 'Highest', 'webinar1')
     ])
+    @pytest.mark.flaky(reruns=3)
     def test_update_issue(self, updated_issue):
         issue = Issue('Webinar (WEBINAR)', 'Bug', 'Bug for update test', 'Highest', 'ValeriiSokolovskyi')
         self.issues_page.create_issue(issue)
