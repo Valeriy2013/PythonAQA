5399c6b681cf0643e9ba4cfbab107552cf0595e4
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2019-04-27 23:47:14 +0300

    fix import

--- JiraSelenium/conftest.py
+++ JiraSelenium/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import allure
 from selenium import webdriver
-from selenium.webdriver.android.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
