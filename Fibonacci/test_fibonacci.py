52aaffd3c98bd8b6821d926a84f9768e9495fd5d
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2019-06-01 09:39:00 +0300

    add random number

--- Fibonacci/test_fibonacci.py
+++ Fibonacci/test_fibonacci.py
@@ -4,6 +4,7 @@ Unit tests for the fibonacci library
 
 import fibonacci
 import pytest
+import random
 
 
 @pytest.mark.fibonacci
@@ -11,3 +12,7 @@ class TestFibonacci:
 
     def test_generateFibonacci(self):
         assert [1, 1, 2, 3, 5, 8, 13] == fibonacci.generateFibonacci(7)
+
+    def test_randomNumber(self):
+        num = random.randint(1,2)
+        assert num == 2
