9ff1a97af284611f611009e604020d8d569ad6f1
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2019-06-01 09:23:35 +0300

    fix

--- Fibonacci/test_fibonacci.py
+++ Fibonacci/test_fibonacci.py
@@ -8,6 +8,6 @@ import pytest
 
 class TestFibonacci:
 
-    @pytest.mark.skip(reason="no way of currently testing this")
+    @pytest.mark.general
     def test_generateFibonacci(self):
         assert [1, 1, 2, 3, 5, 8, 13] == fibonacci.generateFibonacci(7)
