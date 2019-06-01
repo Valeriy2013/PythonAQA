d683182134a3a239d133eff0e6c835aebefed4f5
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2019-06-01 09:47:15 +0300

    fix

--- Fibonacci/test_fibonacci.py
+++ Fibonacci/test_fibonacci.py
@@ -11,8 +11,9 @@ import random
 class TestFibonacci:
 
     def test_generateFibonacci(self):
-        assert [1, 1, 2, 3, 5, 8, 13] == fibonacci.generateFibonacci(7)
+        assert [1, 1, 2, 3, 5, 8, 13] == fibonacci.generateFibonacci(7), "Check fibonacci sequence"
 
+    @pytest.mark.flaky(reruns=5)
     def test_randomNumber(self):
         num = random.randint(1, 2)
-        assert num == 2
+        assert num == 2, "Check random number"
